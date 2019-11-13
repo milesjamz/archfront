b92f7708be744982af25711c8961a94f08f79239
Miles Marillo <mjm03@hampshire.edu>
2019-11-12 23:00:00 -0500

    small changes

--- src/landing.js
+++ src/landing.js
@@ -5,7 +5,7 @@ function Landing(props) {
         <div>
             <h1>Welcome to QuarReport!</h1>
             <strong>The number one way to stay sane in the apocalypse</strong>
-            <p id="regText">Hello, {props.user}! This is a resource that will help you to keep organized and stay productive while the world has ended. Most people are getting fat as fuck, drinking too much, and getting ready to die. But not you, right?!</p>
+            <p id="regText">Hello, {props.user}! This is a resource that will help you to keep organized and stay productive while the world has ended. Most people are wasting this precious time. But not you, right?!</p>
         </div>
     )
 }
