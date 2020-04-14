8c938b9b405e39767f3c439ba96a15f47d71cacb
Miles Marillo <mjm03@hampshire.edu>
2020-04-14 17:48:50 -0400

    updated concept to reflect quarantine journaling

--- src/navbar.js
+++ src/navbar.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="Navbar">
-hello, this is my navbahhH!
-</div>
+Q u a r R e p o r t  ||  calendar  ||  login  ||  logout
+    </div>
 )
 }
 
--- src/selector.js
+++ src/selector.js
@@ -6,29 +6,53 @@ state = {
 	dropDown: ''
 }
 
+// handleOnChange = e => {
+// 	this.setState({dropDown: e.target.value})
+// }
+
+// onSubmit = () => {
+// 	alert(this.state.dropDown)
+// }
+
+// --- below is a dropdown form to be reinserted into render ---
+/* <form onSubmit={this.onSubmit}>
+<label>
+	Pick a Prime Project, Please:
+	<input
+		type='text'
+		value={this.state.dropDown}
+		onChange={this.handleOnChange}
+	/>
+</label>
+</form>  */
+// --- form ends here ---
+
 render() {
 
-	const handleOnChange = e => {
-		this.setState({dropDown: e.target.value})
-	}
-
-	const onSubmit = () => {
-		alert(this.state.dropDown)
-	}
+const today = new Date()
 
   return (
-    <div className="Selector">
-hello, this is my selecktah!
-	<form onSubmit={this.onSubmit}>
-		<label>
-			Pick a Prime Project, Please:
-			<input
-				type='text'
-				value={this.state.dropDown}
-				onChange={this.handleOnChange}
-			/>
-		</label>
-	</form>
+    <div className="selector">
+Hello, USERNAME - today is {today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()}.<br/>
+What did you do today?
+<form>
+algorithms:<br/>
+job applications:<br/>
+blogs:<br/>
+songs:<br/>
+journal pages:<br/>
+personal letters:<br/>
+calls to family:<br/>
+front plank minutes:<br/>
+side plank minutes:<br/>
+rear plank minutes:<br/>
+curls:<br/>
+burpees:<br/>
+push-ups:<br/>
+chin-ups:<br/>
+
+</form>
+
 </div>
 	)}
 }
