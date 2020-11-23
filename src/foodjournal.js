3c5ba3c7dabc20b7dc7919ee93669f2d7f4ec242
Miles Marillo <mjm03@hampshire.edu>
2020-11-23 18:26:38 -0500

    updated some form

--- src/foodjournal.js
+++ src/foodjournal.js
@@ -14,16 +14,24 @@ render() {
     return (
         <div className='eachPage'>
             Here's a list of stuff, ok? <br/>
-            <t>
-            <ul>
-                <li>here's some things</li>
-            </ul>
-            </t>
-            <ol>
-                <li>
-                    here's some other things
-                </li>
-            </ol>
+        There will be a form for your meals today,<br/>
+        and a form for your symptoms today,<br/>
+        and a form for your exercises today.<br/>
+        <button> and buttons to press to certify it all, baybee</button>
+        <form>
+        Here's a meal form<br/>
+        What did you eat?<input label='summary of today' type='text'></input><br/>
+        How big a meal was it?<input label='summary of today' type='text'></input><br/>
+        How fast did you shovel it down?<input label='summary of today' type='text'></input>
+        </form>
+
+        <form>
+        Here's a symptom form<br/>
+        What's wrong?<input label='summary of today' type='text'></input><br/>
+        Care to be more specific?<input label='summary of today' type='text'></input><br/>
+        How long did it last<input label='summary of today' type='text'></input><br/>
+        How severe was it?<input label='summary of today' type='text'></input>
+        </form>
 
         </div>
     )
