53e0e29b0552c2b3de768b929d3f350ee7194705
Miles Marillo <mjm03@hampshire.edu>
2020-11-24 09:37:55 -0500

    improved food form

--- src/foodjournal.js
+++ src/foodjournal.js
@@ -3,14 +3,46 @@ import React from 'react'
 class FoodJournal extends React.Component {
 
     state = {
-        hmm: [],
+        allergens: [],
+        dude: false
     }
 
+    API = 'http://localhost:3000/api/v1/'
+
     componentDidMount() {
-        console.log('fuck dude what the shiet')
+        console.log(`${this.API}allergens`)
+        fetch(`${this.API}allergens`)
+        .then(resp => resp.json())
+        .then(data => this.setState({allergens: data}))
+    }
+
+    handleInputChange() {
+        console.log('dude')
     }
 
 render() {
+
+const allergenBox = (allergen) => {
+    console.log(allergen)
+    return (
+        <div>
+            <label>{allergen.name}</label>
+            <input type='checkbox' 
+                key={allergen.id} 
+                id={allergen.name} 
+                checked={this.state.dude} 
+                onChange={this.handleInputChange} />  
+        </div>
+    )
+}
+
+const addAllergens = () => {
+    console.log('doop de doo')
+    if (this.state.allergens) {
+         return this.state.allergens.map(allergen => allergenBox(allergen) )
+            }
+    }
+
     return (
         <div className='eachPage'>
             Here's a list of stuff, ok? <br/>
@@ -23,7 +55,8 @@ render() {
         What did you eat?<input type='text'></input><br/>
         How big a meal was it?(1 to 10)<input type='number' min='0' max='10' ></input><br/>
         How fast did you shovel it down?(1 to 10)<input type='number' min='0' max='10'></input><br/>
-        What kind of allergens did it have?<input type='number' min='0' max='10'></input><br/>
+        What kind of allergens did it have?<br/>
+        {addAllergens()}
         How many calories did it have?<input type='number' min='0'></input><br/>
         <button>push me push me push me</button>
         </form>
